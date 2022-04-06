1f0da2544a55bf6508ea1817ab47cd9c91e9192f
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2022-04-06 10:56:34 +0100

    feature/setup - updating step

--- step.sh
+++ step.sh
@@ -2,22 +2,14 @@
 # fail if any commands fails
 set -e
 
-STEP_STATUS="failure"
-
-if [ "$BITRISE_BUILD_STATUS" == 0 ]; then
-    STEP_STATUS="success"
-fi
-
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
     -X POST -H 'Accept: application/vnd.github.v3+json' \
     -H 'Authorization: token '"$GITHUB_TOKEN"'' https://api.github.com/repos/hmrc/$BITRISEIO_GIT_REPOSITORY_SLUG/statuses/$BITRISE_GIT_COMMIT \
-    -d '{"state":"'"$STEP_STATUS"'", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
+    -d '{"state":"pending", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
 )
 
 if (($STATUSCODE >= 200 && $STATUSCODE < 300)); then
-    if [ "$STEP_STATUS" == "failure" ]; then
-        exit 1
-    fi
+    exit 0
 else
     echo "curl failed"
     exit 1
