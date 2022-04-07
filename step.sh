c53524f6d950c691f8b14a106621b9a0abcc01ed
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2022-04-07 10:49:07 +0100

    Updating docs

--- step.sh
+++ step.sh
@@ -5,7 +5,7 @@ set -e
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
     -X POST -H 'Accept: application/vnd.github.v3+json' \
     -H 'Authorization: token '"$GITHUB_TOKEN"'' https://api.github.com/repos/hmrc/$BITRISEIO_GIT_REPOSITORY_SLUG/statuses/$BITRISE_GIT_COMMIT \
-    -d '{"state":"pending", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "Pending - '"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
+    -d '{"state":"pending", "target_url": "'"$BITRISE_BUILD_URL"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$BITRISE_TRIGGERED_WORKFLOW_ID"'"}'\
 )
 
 if (($STATUSCODE >= 200 && $STATUSCODE < 300)); then
